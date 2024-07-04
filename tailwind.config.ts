7ab7532ce0bdb4f7852a5b9d7208669bfae0faad
karl-wilson <okosechidelu@gmail.com>
2024-07-04 02:33:26 +0100

    add button color to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        'btnColor':{
+          light: "#ecf1ff",
+          DEFAULT: "#666cff",
+          dark: "#191650"
+        }
+      }
     },
   },
   plugins: [],
