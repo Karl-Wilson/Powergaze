ade50b4992f2b06a2264720d39c518383aa897c0
karl-wilson <okosechidelu@gmail.com>
2024-07-09 12:46:55 +0100

    refactor dropdown using menu component in common folder

--- src/website/landing/header/components/container/dropdown.tsx
+++ src/website/landing/header/components/container/dropdown.tsx
@@ -2,32 +2,19 @@
 import { classJoiner } from "@/src/common/utils/helper"
 import Navbar from "./navbar"
 import NavButtons from "./navButtons"
-import Menu from "../base/menu"
 import { useState } from "react"
+import Menu from "@/src/common/components/core/menu"
 
 type dropdown = {
     class?: string
 }
 
 const Dropdown = (props: dropdown) =>{
-    const [display, setDisplay] = useState<string>("hidden")
-    const click = () =>{
-        if(display == "hidden"){
-            setDisplay("block")
-        }else{
-            setDisplay("hidden")
-        }
-        
-    }
     return(
-        <div className={classJoiner("relative", props.class)}>
-            <Menu click={click}/>
-            <div className={classJoiner("absolute top-10 right-2 px-4 py-6 box-border bg-white drop-shadow-lg w-3/4", display)}>
+        <Menu icon="menu.png" class={props.class} iconClass="w-10 h-10" dropdownClass="top-10 right-2 px-4 py-6 bg-white w-[50vw]">
             <Navbar/>
             <NavButtons/>
-            </div>
-        </div>
-        
+        </Menu>      
     )
 }
 
