0b0f3ab2a5b24b7127bd6cd9e80854d4336d4805
karl-wilson <okosechidelu@gmail.com>
2024-07-09 19:43:48 +0100

    add landing component

---
+++ src/website/landing/view/landing.tsx
@@ -0,0 +1,16 @@
+
+import Feature from "../feature/view/feature";
+import Footer from "../footer/view/footer";
+import Header from "../header/view/header";
+
+const Landing = () =>{
+    return(
+        <>
+            <Header/>
+            <Feature/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Landing;
