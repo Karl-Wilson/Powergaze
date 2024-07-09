a1ba917dd1e94a4aab63dc70b36d4bc4e8637b73
karl-wilson <okosechidelu@gmail.com>
2024-07-09 12:33:48 +0100

    add menu to common components

---
+++ src/common/components/core/menu.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import { classJoiner } from "../../utils/helper"
+
+type menu = {
+    children: React.ReactNode,
+    icon: string,
+    class?: string,
+    iconClass?: string
+    dropdownClass?: string
+}
+
+const Menu = (props: menu) =>{
+    const [display, setDisplay] = useState<string>("hidden")
+    const click = () =>{
+        if(display == "hidden"){
+            setDisplay("block")
+        }else{
+            setDisplay("hidden")
+        }
+        
+    }
+    return(
+        <div className={classJoiner("relative", props.class)}>
+            <img src={props.icon} className={classJoiner("cursor-pointer", props.iconClass)} onClick={click}/>
+            <div className={classJoiner("absolute p-4 box-border drop-shadow-lg", display, props.dropdownClass)}>
+                {props.children}
+            </div>
+    </div>
+    )
+}
+
+export default Menu
