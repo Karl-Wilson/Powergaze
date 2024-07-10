df23d86bab30f40bf0e5e2b72eb65e52e18e06e5
karl-wilson <okosechidelu@gmail.com>
2024-07-10 10:20:43 +0100

    add login layout

---
+++ src/website/auth/login/component/layout/loginLayout.tsx
@@ -0,0 +1,26 @@
+import Header from "../../../common/component/container/header";
+import FormLayoutWrapper from "../../../common/component/core/formLayoutWrapper";
+import HeaderLayoutWrapper from "../../../common/component/core/headerLayoutWrapper";
+import LayoutWrapper from "../../../common/component/core/layoutWrapper";
+import Form from "../containers/form";
+
+type loginLayout = {
+
+}
+
+const LoginLayout = (props: loginLayout) =>{
+    return(
+        <LayoutWrapper>
+            <HeaderLayoutWrapper>
+                <Header title="Sign in" class="col-start-1"/>
+            </HeaderLayoutWrapper>
+            <FormLayoutWrapper>
+                <div className="col-start-2 col-span-2">
+                    <Form/>
+                </div>
+            </FormLayoutWrapper>
+        </LayoutWrapper>
+    )
+}
+
+export default LoginLayout;
