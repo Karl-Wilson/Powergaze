02ad9daa7056bda4e81f02bec2f45c980e63022c
karl-wilson <okosechidelu@gmail.com>
2024-07-10 10:16:08 +0100

    add position and color styling to components

--- src/website/auth/login/component/containers/form.tsx
+++ src/website/auth/login/component/containers/form.tsx
@@ -1,20 +1,23 @@
 import Input from "@/src/common/components/core/form/input"
 import Link from "next/link"
 import FormButtons from "./formButtons"
+import { montserrat } from "@/src/common/utils/fonts"
+import { classJoiner } from "@/src/common/utils/helper"
 
 type form = {
 
 }
 
 const Form  = (props: form) =>{
+    const theme = {borderColor: "border-neutral-200", textColor: "text-neutral-400", bgColor: "bg-white",  borderWidth: "border"}
     return(
-        <form>
-            <div>
-                <Input  name="Email" type="text" label="Email" inputType="default"/>
-                <Input  name="Passowrd" type="password" label="Password" inputType="default"/>
-                <Link href="/reset" className="">Forget Email or Password</Link>
+        <form className="grid grid-cols-1 grid-rows-3">
+            <div className="row-start-1 row-span-2">
+                <Input  name="Email" type="text" label="Email" inputType="default" theme={theme} class="mb-4"/>
+                <Input  name="Passowrd" type="password" label="Password" inputType="default" theme={theme}  class="mb-4"/>
+                <Link href="/reset" className={classJoiner("text-sm", montserrat.className)}>Forget Email or Password</Link>
             </div>
-            <FormButtons/>
+            <FormButtons class="row-start-3"/>
         </form>
     )
 }
--- src/website/auth/login/component/containers/formButtons.tsx
+++ src/website/auth/login/component/containers/formButtons.tsx
@@ -1,5 +1,6 @@
 import Button from "@/src/common/components/core/button"
 import { classJoiner } from "@/src/common/utils/helper"
+import Link from "next/link"
 
 type formButtons = {
     class?: string
@@ -9,14 +10,10 @@ type formButtons = {
 
 const FormButtons = (props: formButtons) =>{
     return(
-        <div className={classJoiner("flex flex-row items-center w-full", props.class)}>
-            <Button label="Create Account" 
-                text={{textColor: "md:text-neutral-800 md:hover:text-btnColor"}} 
-                outline={{borderColor: "border-btnColor", borderWidth: "border md:border-0", textColor: "text-btnColor"}}
-                class="font-bold md:mr-4 w-full md:w-auto" click={props.createAccount}/>
-    
+        <div className={classJoiner("flex flex-row items-center justify-between w-full", props.class)}>
+            <Link href="/signup" className="font-bold">Create Account</Link>
             <Button label="Next" 
-                filled={{bgColor: "bg-btnColor hover: bg-btnColor-dark", textColor: "text-color"}} 
+                filled={{bgColor: "bg-btnColor hover:bg-btnColor-dark", textColor: "text-white"}} 
                 class="font-bold w-full md:w-auto" click={props.submitAccount}/>
         </div>
     )
