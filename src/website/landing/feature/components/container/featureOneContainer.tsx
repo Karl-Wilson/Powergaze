5d9824039ca776a878f9875976978620f013a765
karl-wilson <okosechidelu@gmail.com>
2024-07-12 17:45:06 +0100

    add scrollAnimate to feature one layout components

--- src/website/landing/feature/components/container/featureOneContainer.tsx
+++ src/website/landing/feature/components/container/featureOneContainer.tsx
@@ -11,13 +11,13 @@ const FeatureOneContainer = (props: featureContainer) =>{
     return(
         <Wrapper>
             <div className="col-start-1 md:col-span-2 row-start-2 md:row-start-1">
-                    <FeatureImage src="featureOne.png"/>     
+                    <FeatureImage src="featureOne.png" id="featureOneImage" class="opacity-0"/>     
                 </div>
                 <div className="col-start-1 md:col-start-3 row-start-1 flex flex-col justify-center items-end md:items-center xl:w-[325px] mb-7 md:mb-0">
                     <FeatureContent title="Monitor your Energy Consumption" 
                     body="Lorem Ipsum is simply dummy text of the printing and 
                     typesetting industry. Lorem Ipsum has been the industry's standard 
-                    dummy text ever since the 1500s."/>
+                    dummy text ever since the 1500s." id="featureOneContent" class="opacity-0"/>
                 </div>
         </Wrapper>
     )
--- src/website/landing/feature/components/layout/featureOne.tsx
+++ src/website/landing/feature/components/layout/featureOne.tsx
@@ -1,16 +1,22 @@
-import FeatureContent from "../base/content"
-import FeatureImage from "../base/image"
+import { useRef } from "react"
 import LayoutWrapper from "../base/wrapper/featureLayout"
 import FeatureOneContainer from "../container/featureOneContainer"
-import FeatureContainer from "../container/featureOneContainer"
+import useScrollAnimate from "@/src/common/utils/hooks/scrollAnimate"
+
 
 type featureOne = {
 
 }
 
 const FeatureOne = (props: featureOne) =>{
+    const layout = useRef<HTMLDivElement>(null)
+    const scrollAction = ():void => {
+        document.getElementById("featureOneContent")?.classList.add("animate-staticEaseIn")
+        document.getElementById("featureOneImage")?.classList.add("animate-leftEaseIn")
+  }
+    useScrollAnimate(layout, scrollAction, true)
     return(
-        <LayoutWrapper>
+        <LayoutWrapper layoutRef={layout}>
             <FeatureOneContainer/>
         </LayoutWrapper>
     )
