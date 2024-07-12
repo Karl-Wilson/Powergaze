8517411e8a9e517c1fc331d31eb4b69730d523c2
karl-wilson <okosechidelu@gmail.com>
2024-07-12 18:46:19 +0100

    refactor: rename sectionWrapper to authSectionWrapper

--- src/website/auth/view/login.tsx
+++ src/website/auth/view/login.tsx
@@ -1,5 +1,5 @@
-import SectionWrapper from "@/src/common/components/core/sectionWrapper"
 import LoginLayout from "../component/layout/loginLayout"
+import AuthSectionWrapper from "../component/core/sectionWrapper"
 
 type login = {
 
@@ -7,9 +7,9 @@ type login = {
 
 const LoginPage = (props: login) =>{
     return(
-        <SectionWrapper maxWidth="max-w-[987px]" wrapperClass="h-screen flex flex-col justify-center" innerClass="xl:px-0 mx-0 md:mx-auto w-screen md:w-auto h-screen md:h-auto">
+        <AuthSectionWrapper>
             <LoginLayout/>
-        </SectionWrapper>
+        </AuthSectionWrapper>
     )
 }
 
--- src/website/auth/view/signup.tsx
+++ src/website/auth/view/signup.tsx
@@ -1,5 +1,6 @@
-import SectionWrapper from "@/src/common/components/core/sectionWrapper"
+
 import SignupLayout from "../component/layout/signupLayout"
+import AuthSectionWrapper from "../component/core/sectionWrapper"
 
 type signup = {
 
@@ -7,9 +8,9 @@ type signup = {
 
 const SignupPage = (props: signup) =>{
     return(
-        <SectionWrapper maxWidth="max-w-[987px]" wrapperClass="h-screen flex flex-col justify-center" innerClass="xl:px-0 mx-0 md:mx-auto w-screen md:w-auto h-screen md:h-auto">
+        <AuthSectionWrapper>
             <SignupLayout/>
-        </SectionWrapper>
+        </AuthSectionWrapper>
     )
 }
 
