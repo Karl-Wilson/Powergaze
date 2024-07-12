497ddda3b3eed8b07eb99c58112220dd8d1c5aac
karl-wilson <okosechidelu@gmail.com>
2024-07-12 18:35:11 +0100

    update layout size to full screen

--- src/website/auth/component/core/layoutWrapper.tsx
+++ src/website/auth/component/core/layoutWrapper.tsx
@@ -4,7 +4,7 @@ type layout = {
 
 const LayoutWrapper = (props: layout) =>{
     return(
-        <div className="md:rounded-[42px] bg-white p-10 box-border w-full md:w-[754px]">
+        <div className="md:rounded-[42px] bg-white p-10 box-border w-full md:w-[754px] h-screen md:h-auto flex flex-col justify-center md:flex-none">
             {props.children}
         </div>
     )
--- src/website/auth/view/signup.tsx
+++ src/website/auth/view/signup.tsx
@@ -7,7 +7,7 @@ type signup = {
 
 const SignupPage = (props: signup) =>{
     return(
-        <SectionWrapper maxWidth="max-w-[987px]" wrapperClass="h-screen flex flex-col justify-center" innerClass="xl:px-0">
+        <SectionWrapper maxWidth="max-w-[987px]" wrapperClass="h-screen flex flex-col justify-center" innerClass="xl:px-0 mx-0 md:mx-auto w-screen md:w-auto h-screen md:h-auto">
             <SignupLayout/>
         </SectionWrapper>
     )
