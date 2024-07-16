f040f3f009d50679a48f64852f8090b9d5aee7b7
karl-wilson <okosechidelu@gmail.com>
2024-07-16 08:04:22 +0100

    update compnent height

--- src/web_app/main/view/main.tsx
+++ src/web_app/main/view/main.tsx
@@ -2,7 +2,7 @@
 
 const Main = () =>{
     return(
-        <section className="w-full h-full p-14 box-border overflow-auto bg-neutral-600">
+        <section className="w-full h-[calc(100vh-80px)] p-14 box-border overflow-auto bg-neutral-600">
             
         </section>
     )
