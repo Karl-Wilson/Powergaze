a68aacb6e1b64ccc6c614e90025ef32158c3de6f
karl-wilson <okosechidelu@gmail.com>
2024-07-16 07:40:37 +0100

    add main view component

---
+++ src/web_app/main/view/main.tsx
@@ -0,0 +1,11 @@
+
+
+const Main = () =>{
+    return(
+        <section className="w-full h-full p-14 box-border overflow-auto bg-neutral-600">
+            
+        </section>
+    )
+}
+
+export default Main
