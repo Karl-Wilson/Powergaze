87670f28f9409309f14d51164dc45b5bcebc0540
karl-wilson <okosechidelu@gmail.com>
2024-07-16 08:05:35 +0100

    update component height

--- src/web_app/navigation/view/Navigation.tsx
+++ src/web_app/navigation/view/Navigation.tsx
@@ -2,7 +2,7 @@ import NavLayout from "../component/layout/navLayout"
 
 const Navigation = () =>{
     return(
-        <nav className="w-full h-[80px] box-border p-3 flex flex-row items-center bg-dashboard-primary">
+        <nav className="w-full h-20 box-border p-3 flex flex-row items-center bg-dashboard-primary">
             <NavLayout/>
         </nav>
     )
