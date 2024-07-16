0a93e081b4396687d6e344e9f020537e30d84e2b
karl-wilson <okosechidelu@gmail.com>
2024-07-16 14:48:39 +0100

    add dashboard view component

---
+++ src/web_app/main/energy_plan/view/energy_plan.tsx
@@ -0,0 +1,8 @@
+const EnergyPlan =( ) =>{
+    return(
+        <div>
+            Energy plan
+        </div>
+    )
+}
+export default EnergyPlan
--- src/web_app/main/view/main.tsx
+++ src/web_app/main/view/main.tsx
@@ -1,9 +1,10 @@
+import Dashboard from "../dashboard/view/dashboard"
 
 
 const Main = () =>{
     return(
         <section className="w-full h-[calc(100vh-80px)] p-14 box-border overflow-auto bg-neutral-600">
-            
+            <Dashboard/>
         </section>
     )
 }
