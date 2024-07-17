3f1e56db20ae32c91b2e175ac129a2ad95f2f450
karl-wilson <okosechidelu@gmail.com>
2024-07-17 09:00:56 +0100

    add redux store to layout

--- app/(web app)/app/layout.tsx
+++ app/(web app)/app/layout.tsx
@@ -1,13 +1,18 @@
+"use client"
+import { store } from "@/src/web_app/common/store/store"
 import DashboardView from "@/src/web_app/view/dashboardView"
+import { Provider } from "react-redux"
 
 type dashLayout = {
     children: React.ReactNode
 }
 const DashboardLayout = (props:dashLayout) =>{
     return(
-        <DashboardView>
-            {props.children}
-        </DashboardView>
+        <Provider store={store}>
+            <DashboardView>
+                {props.children}
+            </DashboardView>
+        </Provider>
     )
 }
 export default DashboardLayout
