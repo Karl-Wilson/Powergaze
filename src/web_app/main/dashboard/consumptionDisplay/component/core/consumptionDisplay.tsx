3bf9745cc25495faac4b53184f894ac8a056360a
karl-wilson <okosechidelu@gmail.com>
2024-07-22 05:51:32 +0100

    update: add style classes to consumption component

--- src/web_app/main/dashboard/consumptionDisplay/component/core/consumptionDisplay.tsx
+++ src/web_app/main/dashboard/consumptionDisplay/component/core/consumptionDisplay.tsx
@@ -12,11 +12,14 @@ type consumption = {
 }
 const ConsumptionDisplay = (props:consumption) =>{
     return(
-        <div className={classJoiner("rounded-xl py-3 px-6 relative text-white", props.class, lato.className)}>
+        <div className={classJoiner("rounded-xl py-3 px-6 relative text-white flex flex-col", props.class, lato.className)}>
             <div className={classJoiner("text-xl z-20")}>{props.title}</div>
-            <div className="flex flex-row items-center z-20">
-                <span className={classJoiner("text-4xl", props.valueClass)}>{props.value}</span>
-                {props.unit && <span className={classJoiner("text-3xl", props.unitClass)}>{props.unit}</span>}
+            <div className="flex flex-row items-center z-20 h-full w-full absolute top-0 left-0 py-3 px-6 box-border">
+                <div className="flex flex-row items-end font-bold break-words">
+                    <span className={classJoiner("text-4xl", props.valueClass)}>{props.value}</span>
+                    {props.unit && <span className={classJoiner("text-2xl", props.unitClass)}>{props.unit}</span>}
+                </div>
+                
             </div>
             <img src={props.icon} className="absolute z-10 bottom-0 right-0 w-36 h-auto"/>
         </div>
