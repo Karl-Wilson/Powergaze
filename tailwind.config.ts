56a15213f6c824f2546ed468cd05d42124afdf6e
karl-wilson <okosechidelu@gmail.com>
2024-07-24 10:30:35 +0100

    update: add red color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,8 @@ const config: Config = {
         'bgWebsite': '#0A072B',
         'dashboard': {
           primary: '#1C1A3F',
-          green: '#2BC24A'
+          green: '#2BC24A',
+          red: "#FF6E91"
         }
       },
       spacing:{
