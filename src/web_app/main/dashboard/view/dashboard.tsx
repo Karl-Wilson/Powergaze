54266ab1e4db41649ea05a565abcb678b9311bfd
karl-wilson <okosechidelu@gmail.com>
2024-07-24 13:59:49 +0100

    update: changed wrapper div to gridWrapper

--- src/web_app/main/dashboard/view/dashboard.tsx
+++ src/web_app/main/dashboard/view/dashboard.tsx
@@ -1,3 +1,4 @@
+import GridWrapper from "../../common/components/core/gridWrapper"
 import ApplianceStatus from "../applianceStatus/component/layout/applianceStatus"
 import ApplianceUsage from "../applianceUsage/component/layout/applianceUsage"
 import ConsumptionLayout from "../consumptionDisplay/component/layout/consumptionLayout"
@@ -5,12 +6,12 @@ import Usage from "../usage/component/layout/usage"
 
 const Dashboard = () =>{
     return(
-        <div className="grid grid-cols-4 md:grid-cols-12 gap-6">
+        <GridWrapper>
             <ConsumptionLayout class="col-start-1 col-span-4 md:col-span-12 row-start-1"/>
             <Usage class="row-start-2 col-start-1 col-span-4 md:col-span-7"/>
             <ApplianceUsage class="row-start-3 col-start-1 col-span-4 md:row-start-2 md:col-start-8 md:col-span-5"/>
             <ApplianceStatus class="row-start-4 col-start-1 col-span-4 md:row-start-3 md:col-span-12"/>
-        </div>
+        </GridWrapper>
     )
 }
 
