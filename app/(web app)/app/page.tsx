6a6b254262731c8b6c626df6def49b7b6a2d94be
karl-wilson <okosechidelu@gmail.com>
2024-07-27 04:11:45 +0100

    add: added dahsboard component to dashboard page route

---
+++ app/(web app)/app/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import Dashboard from "@/src/web_app/main/dashboard/view/dashboard";
+
+export default function Home() {
+  return (
+    <main>
+      <Dashboard/>
+    </main>
+  );
+}
