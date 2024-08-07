25c8539ee5131acf216dc941113a9187eb7be401
karl-wilson <okosechidelu@gmail.com>
2024-08-07 13:57:10 +0100

    update: add error message component and custom hooks for authentication

--- src/website/auth/component/layout/loginLayout.tsx
+++ src/website/auth/component/layout/loginLayout.tsx
@@ -1,9 +1,13 @@
+"use client"
 import Header from "../container/header";
 import Form from "../container/form";
 import FormLayoutWrapper from "../core/formLayoutWrapper";
 import HeaderLayoutWrapper from "../core/headerLayoutWrapper";
 import LayoutWrapper from "../core/layoutWrapper";
 import InputGroup from "../container/inputGroup";
+import ErrorMsg from "../core/errorMsg";
+import useLogin from "../../persistence/login";
+
 
 type loginLayout = {
 
@@ -11,14 +15,17 @@ type loginLayout = {
 
 const LoginLayout = (props: loginLayout) =>{
     const Page = "login"
+    const {error, submit, change} = useLogin()
+
     return(
         <LayoutWrapper>
             <HeaderLayoutWrapper>
                 <Header title="Sign in" class="col-start-1"/>
             </HeaderLayoutWrapper>
             <FormLayoutWrapper>
-                <Form class="col-start-1 md:col-start-2 md:col-span-2" page={Page}>
-                    <InputGroup page={Page}/>
+                <Form class="col-start-1 md:col-start-2 md:col-span-2" page={Page} id="login" submit={submit}>
+                    {error && <ErrorMsg error={error} class="mb-6"/>}
+                    <InputGroup page={Page} error={error} change={change}/>
                 </Form>
             </FormLayoutWrapper>
         </LayoutWrapper>
--- src/website/auth/component/layout/signupLayout.tsx
+++ src/website/auth/component/layout/signupLayout.tsx
@@ -4,6 +4,8 @@ import FormLayoutWrapper from "../core/formLayoutWrapper";
 import HeaderLayoutWrapper from "../core/headerLayoutWrapper";
 import LayoutWrapper from "../core/layoutWrapper";
 import InputGroup from "../container/inputGroup";
+import useSignup from "../../persistence/signup";
+import ErrorMsg from "../core/errorMsg";
 
 type signupLayout = {
 
@@ -11,14 +13,17 @@ type signupLayout = {
 
 const SignupLayout = (props: signupLayout) =>{
     const Page = "signup"
+    const {error, submit, change} = useSignup()
+
     return(
         <LayoutWrapper>
             <HeaderLayoutWrapper>
                 <Header title="Signup" class="col-start-1"/>
             </HeaderLayoutWrapper>
             <FormLayoutWrapper>
-                <Form class="col-start-1 md:col-start-2 md:col-span-2" page={Page}>
-                    <InputGroup page={Page}/>
+                <Form class="col-start-1 md:col-start-2 md:col-span-2" page={Page} submit={submit}>
+                {error && <ErrorMsg error={error} class="mb-6"/>}
+                    <InputGroup page={Page} error={error} change={change}/>
                 </Form>
             </FormLayoutWrapper>
         </LayoutWrapper>
