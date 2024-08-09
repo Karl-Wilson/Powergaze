564a720460e4b4fc41a74e908dd3fe62de82b521
karl-wilson <okosechidelu@gmail.com>
2024-08-09 15:13:28 +0100

    update: added dataState component and charts

--- src/web_app/main/dashboard/applianceUsage/component/layout/applianceUsage.tsx
+++ src/web_app/main/dashboard/applianceUsage/component/layout/applianceUsage.tsx
@@ -1,13 +1,27 @@
+import { PieChart } from "@/src/web_app/main/common/charts/charts"
 import DashboardCard, { CardBody, CardHeader } from "@/src/web_app/main/common/components/core/card"
+import DataState from "@/src/web_app/main/common/components/core/dataState"
 
 type usage = {
     class?: string
 }
 const ApplianceUsage = (props: usage) =>{
+    let data: any[] = [
+        ["Task", "Hours per Day"],
+        ["Laptop", 11],
+        ["Television", 2],
+        ["Bulbs", 2],
+        ["Refrigerator TV", 2],
+        ["Washer", 7],
+    ]
     return(
         <DashboardCard class={props.class}>
             <CardHeader title="Appliance Usage"/>
-            <CardBody><div></div></CardBody>
+            <CardBody>
+                <DataState data={data}>
+                    <PieChart data={data}/>
+                </DataState>
+            </CardBody>
         </DashboardCard>
     )
 }
--- src/web_app/main/dashboard/usage/component/layout/usage.tsx
+++ src/web_app/main/dashboard/usage/component/layout/usage.tsx
@@ -1,19 +1,30 @@
 import { classJoiner } from "@/src/common/utils/helper"
+import { AreaChart, BarChart } from "@/src/web_app/main/common/charts/charts"
 import DashboardCard, { CardBody, CardHeader } from "@/src/web_app/main/common/components/core/card"
 import CardSelect from "@/src/web_app/main/common/components/core/cardSelect"
+import DataState from "@/src/web_app/main/common/components/core/dataState"
 
 type usage = {
     class?: string
 }
 const Usage = (props: usage) =>{
     let val = ["Yearly", "Monthly", "Daily"]
+    let data: any[] = [
+        ["Year", "Sales", "Expenses"],
+        ["2013", 1000, 400],
+        ["2014", 1170, 460],
+        ["2015", 660, 1120],
+        ["2016", 1030, 540],
+      ];
     return(
         <DashboardCard class={classJoiner(props.class)}>
             <CardHeader title="KWh Usage">
                 <CardSelect values={val}/>
             </CardHeader>
             <CardBody>
-                <div></div>
+                <DataState data={data}>
+                    <AreaChart data={data}/>
+                </DataState>
             </CardBody>
         </DashboardCard>
     )
