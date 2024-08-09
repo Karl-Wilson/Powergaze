c75b7ff1ebe105d17587a069dc6781023265f843
karl-wilson <okosechidelu@gmail.com>
2024-08-09 09:39:03 +0100

    update: removed authorization codes from page components

--- app/(website)/(auth)/login/page.tsx
+++ app/(website)/(auth)/login/page.tsx
@@ -1,29 +1,14 @@
 "use client"
 import LoginPage from "@/src/website/auth/view/login"
 import PageWrapper from "@/src/website/common/component/pageWrapper"
-import { useContext, useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { UserContext } from "../layout";
-
 
 
 const Login = () =>{
-    const user = useContext(UserContext)
-    const router = useRouter()
-  
-    if(user == false){
         return(
             <PageWrapper>
                 <LoginPage/>
             </PageWrapper>
         )
-    }else if(user){
-        //redirect 
-        router.push("/app")
-        return(<div>loading inner</div>)
-    }
-      return(<div>loading outer</div>)
-    
 }
 
 export default Login
--- app/(website)/(auth)/signup/page.tsx
+++ app/(website)/(auth)/signup/page.tsx
@@ -1,26 +1,13 @@
 "use client"
 import SignupPage from "@/src/website/auth/view/signup"
 import PageWrapper from "@/src/website/common/component/pageWrapper"
-import { useContext } from "react"
-import { UserContext } from "../layout"
-import { useRouter } from "next/navigation"
 
 const Signup = () =>{
-    const user = useContext(UserContext)
-    const router = useRouter()
-  
-    if(user == false){
         return(
             <PageWrapper>
                 <SignupPage/>
             </PageWrapper>
         )
-    }else if(user){
-        //redirect 
-        router.push("/app")
-        return(<div>loading inner</div>)
-    }
-      return(<div>loading outer</div>)
 }
 
 export default Signup
