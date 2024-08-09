19f5af904a5608cd1c04276686c7a7db346cccd9
karl-wilson <okosechidelu@gmail.com>
2024-08-09 18:16:13 +0100

    update: add change handler. add default select option

--- src/web_app/main/common/components/core/cardSelect.tsx
+++ src/web_app/main/common/components/core/cardSelect.tsx
@@ -1,16 +1,23 @@
 import { lato } from "@/src/common/utils/fonts"
 import { classJoiner } from "@/src/common/utils/helper"
+import { ChangeEventHandler, LegacyRef } from "react"
 
 type select = {
     values: string[] | number[]
     class?: string
+    selectRef?: LegacyRef<HTMLSelectElement>
+    change?: ChangeEventHandler
+    default?: string
 }
 const CardSelect = (props:select) =>{
     return(
         <div className={classJoiner(props.class, "")}>
-            <select className={classJoiner("w-full h-full border border-neutral-300 border-solid py-2 px-4 box-border rounded-xl", lato.className)}>
+            <select className={classJoiner("w-full h-full border border-neutral-300 border-solid py-2 px-4 box-border rounded-xl", lato.className)} ref={props.selectRef} onChange={props.change}>
                 {props.values.map(item=>{
-                    return <option key={item as string + Math.random()}>{item}</option>
+                    if(item == props.default){
+                        return <option key={item as string + Math.random()} selected>{item}</option>
+                    }
+                    return <option key={item as string + Math.random()}>{item}</option> 
                 })}
             </select>
         </div>
