d38dceb724a8b613fbcae0c8e22ced92c22ae115
karl-wilson <okosechidelu@gmail.com>
2024-08-17 11:10:11 +0100

    update: removed action function. added loadData hook. add dataState component

--- src/web_app/main/appliance/appliance/component/layout/applianceLayout.tsx
+++ src/web_app/main/appliance/appliance/component/layout/applianceLayout.tsx
@@ -5,18 +5,24 @@ import DashboardCard, { CardBody, CardHeader } from "@/src/web_app/main/common/c
 import ApplianceList from "../container/applianceList"
 import { useSelector } from "react-redux"
 import { RootState } from "@/src/web_app/common/store/store"
+import useLoadData from "@/src/web_app/main/common/persistence/loadData"
+import { setApplianceStatus } from "@/src/web_app/common/store/reducer/dashboardReducer"
+import DataState from "@/src/web_app/main/common/components/core/dataState"
 
 type layout = {
     class?: string
 }
 const ApplianceLayout = (props: layout) =>{
     const data = useSelector((state:RootState)=>state.dashboard.applianceStatus)
-    const action = () =>{}
+    const {isLoading} = useLoadData("/api/applianceStatus", setApplianceStatus)
+
     return(
         <DashboardCard class={props.class}>
-            <CardHeader title="Appliance"/>
-            <CardBody class="overflow-hidden hover:overflow-auto max-h-64">
-                <ApplianceList list={data} action={action}/>
+            <CardHeader title="Appliance" isLoading={isLoading}/>
+            <CardBody class="overflow-hidden hover:overflow-auto max-h-64" isLoading={isLoading} loadingClass="h-[200px]">
+                <DataState data={data}>
+                    <ApplianceList list={data}/>
+                </DataState>
             </CardBody>
         </DashboardCard>
     )
