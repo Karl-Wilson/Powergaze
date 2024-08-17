12b16c30220e5a65e8244d8ad75f7df8b38c5d59
karl-wilson <okosechidelu@gmail.com>
2024-08-17 15:02:00 +0100

    update: add link to logo

--- src/web_app/sidebar/component/container/sidebarHeader.tsx
+++ src/web_app/sidebar/component/container/sidebarHeader.tsx
@@ -3,12 +3,13 @@ import { RootState } from "@/src/web_app/common/store/store"
 import { classJoiner } from "@/src/common/utils/helper"
 import useSidemenu from "@/src/web_app/common/hooks/sidemenu"
 import { useSelector } from "react-redux"
+import Link from "next/link"
 
 const SidebarHeader = () =>{
     const {isMenuVisible} = useSidemenu()
     return(
         <div className={classJoiner("flex flex-row items-center justify-center w-full h-20 box-border border-b border-btnColor border-solid transition-all md:p-3", isMenuVisible? "p-3" : "p-0")}>
-            <Logo logo="../dash_logo.png" class="w-36 h-auto"/>
+            <Link href="/"><Logo logo="../dash_logo.png" class="w-36 h-auto"/></Link>
         </div>
     )
 }
