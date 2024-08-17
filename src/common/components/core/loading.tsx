ef30ded925c7ee45b90133829b6de7cf593b51b7
karl-wilson <okosechidelu@gmail.com>
2024-08-17 14:56:50 +0100

    update: changed image path

--- src/common/components/core/loading.tsx
+++ src/common/components/core/loading.tsx
@@ -1,7 +1,7 @@
 const Loading = () =>{
     return(
         <div className="absolute top-0 left-0 z-20 flex flex-col flex-row items-center justify-center bg-white w-full h-full">
-            <img src="loading.gif" className="w-36"/>
+            <img src="../loading.gif" className="w-36"/>
         </div>
     )
 }
