bfc1237a4a1308b8ef8d89fc01812df980743148
karl-wilson <okosechidelu@gmail.com>
2024-08-18 23:17:57 +0100

    update: added class props

--- src/website/common/component/pageWrapper.tsx
+++ src/website/common/component/pageWrapper.tsx
@@ -1,10 +1,13 @@
+import { classJoiner } from "@/src/common/utils/helper"
+
 type pageWrapper = {
     children: React.ReactNode
+    class?: string
 }
 
 const PageWrapper = (props: pageWrapper) =>{
     return(
-        <main className="md:px-4 bg-bgWebsite md:pt-3">
+        <main className={classJoiner("md:px-4 bg-bgWebsite md:pt-3", props.class)}>
             {props.children}
         </main>
     )
