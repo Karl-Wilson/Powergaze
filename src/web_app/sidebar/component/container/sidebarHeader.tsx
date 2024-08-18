ae91d0b10e0fea0699b84d80684d9f3657b81787
karl-wilson <okosechidelu@gmail.com>
2024-08-18 23:42:05 +0100

    update: add width class to medium screen. change border color

--- src/web_app/sidebar/component/container/sidebarHeader.tsx
+++ src/web_app/sidebar/component/container/sidebarHeader.tsx
@@ -8,8 +8,8 @@ import Link from "next/link"
 const SidebarHeader = () =>{
     const {isMenuVisible} = useSidemenu()
     return(
-        <div className={classJoiner("flex flex-row items-center justify-center h-20 box-border border-b border-btnColor border-solid transition-all md:p-3", isMenuVisible? "p-3 w-full" : "p-0 w-0")}>
-            <Link href="/"><Logo logo="../dash_logo.png" class={classJoiner("h-auto", isMenuVisible? "w-36" : "w-0" )}/></Link>
+        <div className={classJoiner("flex flex-row items-center justify-center h-20 box-border border-b border-[#54517C] border-solid transition-all md:p-3 md:w-full", isMenuVisible? "p-3 w-full" : "p-0 w-0")}>
+            <Link href="/"><Logo logo="../dash_logo.png" class={classJoiner("h-auto md:w-36", isMenuVisible? "w-36" : "w-0" )}/></Link>
         </div>
     )
 }
