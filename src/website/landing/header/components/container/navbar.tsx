72a692c5bf1aebf6f94dde0302a7a971fac67fcd
karl-wilson <okosechidelu@gmail.com>
2024-08-18 23:43:44 +0100

    update: remove contact from navbar

--- src/website/landing/header/components/container/navbar.tsx
+++ src/website/landing/header/components/container/navbar.tsx
@@ -5,7 +5,7 @@ type navbar = {
     class?: string
 }
 const Navbar = (props: navbar) =>{
-    let value = [{name: "Home", url: "/"}, {name: "About", url: "/about"}, {name: "Contact", url: "/contact"}]
+    let value = [{name: "Home", url: "/"}, {name: "About", url: "/about"}]
     return(
         <Nav menu={value} class={classJoiner("flex flex-col md:flex-row items-center w-full px-4", props.class)} linkClass="py-2 md:pr-8 hover:text-btnColor"/>
     )
