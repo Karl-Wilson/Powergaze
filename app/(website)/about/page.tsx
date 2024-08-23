4f24e1d972ded97b6584330b9b1edddd252e06a1
karl-wilson <okosechidelu@gmail.com>
2024-08-23 05:51:15 +0100

    update: added page title

--- app/(website)/about/page.tsx
+++ app/(website)/about/page.tsx
@@ -1,6 +1,10 @@
 import AboutPage from "@/src/website/about/view/about"
 import PageWrapper from "@/src/website/common/component/pageWrapper"
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+    title: "About",
+  };
 const About = () =>{
     return(
         <PageWrapper class="min-h-screen pb-3">
