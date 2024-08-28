f2e00a3d4fb7341f773827015dd14f0802283850
karl-wilson <okosechidelu@gmail.com>
2024-08-28 16:52:43 +0100

    update: added padding bottom to skeleton

--- src/web_app/main/dashboard/consumptionDisplay/component/core/loading.tsx
+++ src/web_app/main/dashboard/consumptionDisplay/component/core/loading.tsx
@@ -9,7 +9,7 @@ const LayoutLoading = () =>{
         <div className="grid grid-cols-4 md:grid-cols-12 gap-x-2 lg:gap-x-6 mt-6">
            <Skeleton containerClassName=" col-start-1 col-span-4 md:col-start-1 md:col-span-4 h-[179px] pb-3" borderRadius={13} className="h-full"/> 
            <Skeleton containerClassName="col-start-1 col-span-4 md:col-start-5 md:col-span-4 h-[179px] pb-3" borderRadius={13} className="h-full"/> 
-           <Skeleton containerClassName="col-start-1 col-span-4 md:col-start-9 md:col-span-4 h-[179px]" borderRadius={13} className="h-full"/> 
+           <Skeleton containerClassName="col-start-1 col-span-4 md:col-start-9 md:col-span-4 h-[179px] pb-3" borderRadius={13} className="h-full"/> 
         </div>
         
         </>
