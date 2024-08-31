2d201434e86d831a92b507d1ac5c530fb461f1c5
karl-wilson <okosechidelu@gmail.com>
2024-08-31 16:14:38 +0100

    update: added max-width to components

--- src/website/landing/footer/view/footer.tsx
+++ src/website/landing/footer/view/footer.tsx
@@ -9,7 +9,7 @@ type footer = {
 const Footer = (props: footer) =>{
     return(
         <footer>
-            <SectionWrapper maxWidth="max-w-6xl" wrapperClass="md:rounded-t-[42px] bg-white py-8">
+            <SectionWrapper maxWidth="max-w-6xl" wrapperClass="md:rounded-t-[42px] bg-white py-8 max-w-[1920px] mx-auto">
                 <FooterLayout/>
             </SectionWrapper>
             
--- src/website/landing/header/view/header.tsx
+++ src/website/landing/header/view/header.tsx
@@ -4,7 +4,7 @@ import Heroarea from "../components/layout/heroarea";
 
 const Header = () =>{
     return(
-        <SectionWrapper maxWidth="max-w-6xl" wrapperClass="md:rounded-[42px] bg-white md:my-3" innerClass="px-4 xl:px-0">
+        <SectionWrapper maxWidth="max-w-6xl" wrapperClass="md:rounded-[42px] bg-white md:my-3 max-w-[1920px] mx-auto" innerClass="px-4 xl:px-0">
             <Navigation/>
             <Heroarea/>
         </SectionWrapper>
